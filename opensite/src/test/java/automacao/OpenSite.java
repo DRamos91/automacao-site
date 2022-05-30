1ab4d6c23e1140757dccec393337486e2bed628d
Daniel Moreno <danielmrg@gmail.com>
2022-05-30 17:54:08 -0300

    Mehora na parte de buscar informações no site

--- opensite/src/test/java/automacao/OpenSite.java
+++ opensite/src/test/java/automacao/OpenSite.java
@@ -1,5 +1,7 @@
 package automacao;
 
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,6 +10,9 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class OpenSite {
 
@@ -15,34 +20,60 @@ public class OpenSite {
 
 	@Before
 	public void setUp() throws Exception {
+		// Atualiza e roda com a versão mais atualizada do webdriver
+		WebDriverManager.chromedriver().setup();
+		// ChromeDriver driver = new ChromeDriver();
+
 		// Informando ao sistema o local do driver do google chrome
-		System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver.exe");
-		driver = new ChromeDriver();
-		
+		// System.setProperty("webdriver.chrome.driver",
+		// "C:\\drivers\\chromedriver.exe");
 
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		// Comando para fechar o navegador
-	//	 driver.quit();
+		 driver.quit();
 	}
 
 	@Test
 	public void test() {
 		// Comando para acessar o site desejado
-		driver.get("https://www.americanas.com.br/");
+		ChromeOptions options = new ChromeOptions();
+		driver = new ChromeDriver(options);
+		driver.get("https://www.americanas.com.br");
+		//exibe o titulo da pagina
+		String title = driver.getTitle();
+		System.out.println(title);
 
-		WebElement campoPesquisado = driver.findElement(By.name("conteudo"));
-		campoPesquisado.clear();
-		campoPesquisado.sendKeys("Webcam", Keys.ENTER);
+		//digita webcam na barra de pesquisa do site
+		WebElement searchBox = driver.findElement(By.name("conteudo"));
+		searchBox.clear();
+		searchBox.sendKeys("WebCam", Keys.ENTER);
+
 		
-		String preco = driver.findElement(By.xpath("//*[@id=\"rsyswpsdk\"]")).getText();
+//		String preco = driver.findElement(By.xpath("//*[@id=\"rsyswpsdk\"]/div/main/div/div[3]/div[2]")).getText();
+		List<WebElement> elements = driver.findElements(By.xpath("//*[@id=\"rsyswpsdk\"]/div/main/div/div[3]/div[2]"));
 
-		System.out.println(preco.toLowerCase().contains("webcam".toLowerCase()));
+		for (WebElement element : elements) {
+		    System.out.println("Paragraph text:" + element.getText());
+		}
+				
+//		System.out.println(preco);
+		
+		
 
-		System.out.println("O titulo da pagina é: " + driver.getTitle());
+//		driver.get("https://www.americanas.com.br/");
 
+//		WebElement campoPesquisado = driver.findElement(By.name("conteudo"));
+//		campoPesquisado.clear();
+//		campoPesquisado.sendKeys("Webcam", Keys.ENTER);
+
+//		String preco = driver.findElement(By.xpath("//*[@id=\"rsyswpsdk\"]")).getText();
+
+//		System.out.println(preco.toLowerCase().contains("webcam".toLowerCase()));
+
+//		System.out.println("O titulo da pagina é: " + driver.getTitle());
 
 	}
 
